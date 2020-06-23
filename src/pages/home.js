13eeb8d7958d611600a2d9b7743e586583c31382
Kamrul Hossain <kam.hossain5@gmail.com>
2020-06-22 22:08:01 -0400

    Displayed posts in cards in the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import Post from '../components/Post'
 
 class home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
-        )
-    }
+  state = {
+    posts: null
+  };
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    let recentPostsMarkup = this.state.posts ? (
+      this.state.posts.map(post => <Post post={post}/>)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentPostsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default home;
