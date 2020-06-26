bdef7381916b8405e988e1d2158fba12e87a6985
Kamrul Hossain <kam.hossain5@gmail.com>
2020-06-26 18:16:48 -0400

    Used a sign up redux action to send the signup request and redirected to home page if authenticated

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import jwtDecode from "jwt-decode";
 // Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
 
 // Components
 import Navbar from "./components/Navbar";
@@ -18,18 +20,20 @@ import AuthRoute from "./util/AuthRoute";
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import axios from "axios";
 
 const theme = createMuiTheme(themeFile);
 
-let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser())
     window.location.href = "/login";
-    authenticated = false;
   } else {
-    authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData());
   }
 }
 
@@ -47,13 +51,11 @@ class App extends Component {
                   exact
                   path="/login"
                   component={login}
-                  authenticated={authenticated}
                 />
                 <AuthRoute
                   exact
                   path="/signup"
                   component={signup}
-                  authenticated={authenticated}
                 />
               </Switch>
             </div>
