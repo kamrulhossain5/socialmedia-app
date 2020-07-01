e6800109df3a541692cfe4dd1bbcb37faaff9411
Kamrul Hossain <kam.hossain5@gmail.com>
2020-07-01 17:14:35 -0400

    Created actions for liking and unliking a post and showed different buttons depending on liked status

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,12 +3,23 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import PropTypes from "prop-types";
+import MyButton from "../util/MyButton";
 
 // MUI Stuff
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+
+// Icons
+import ChatIcon from "@material-ui/icons/Chat";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
+
+// Redux
+import { connect } from "react-redux";
+import { likePost, unlikePost } from "../redux/actions/dataActions";
 
 const styles = {
   card: {
@@ -25,6 +36,20 @@ const styles = {
 };
 
 class Post extends Component {
+  likedPost = () => {
+    if (
+      this.props.user.likes &&
+      this.props.user.likes.find(like => like.postId === this.props.post.postId)
+    )
+      return true;
+    else return false;
+  };
+  likePost = () => {
+    this.props.likePost(this.props.post.postId);
+  };
+  unlikePost = () => {
+    this.props.unlikePost(this.props.post.postId);
+  };
   render() {
     dayjs.extend(relativeTime);
     const {
@@ -37,8 +62,24 @@ class Post extends Component {
         postId,
         likeCount,
         commentCount
-      }
+      },
+      user: { authenticated }
     } = this.props;
+    const likeButton = !authenticated ? (
+      <MyButton tip="Like">
+        <Link to="/login">
+          <FavoriteBorder color="primary" />
+        </Link>
+      </MyButton>
+    ) : this.likedPost() ? (
+      <MyButton tip="Undo like" onClick={this.unlikePost}>
+        <FavoriteIcon color="primary" />
+      </MyButton>
+    ) : (
+      <MyButton tip="Like" onClick={this.likePost}>
+        <FavoriteBorder color="primary" />
+      </MyButton>
+    );
     return (
       <Card className={classes.card}>
         <CardMedia
@@ -59,10 +100,36 @@ class Post extends Component {
             {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
+          {likeButton}
+          <span>{likeCount} Likes</span>
+          <MyButton tip="comments">
+            <ChatIcon color="primary" />
+          </MyButton>
+          <span>{commentCount} comments</span>
         </CardContent>
       </Card>
     );
   }
 }
 
-export default withStyles(styles)(Post);
+Post.propTypes = {
+  likePost: PropTypes.func.isRequired,
+  unlikePost: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+const mapActionsToProps = {
+  likePost,
+  unlikePost
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(Post));
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,27 +1,22 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
 
 import Post from "../components/Post";
-import Profile from '../components/Profile'
+import Profile from "../components/Profile";
+
+import { connect } from "react-redux";
+import { getPosts } from "../redux/actions/dataActions";
 
 class home extends Component {
-  state = {
-    posts: null
-  };
   componentDidMount() {
-    axios
-      .get("/posts")
-      .then(res => {
-        this.setState({
-          posts: res.data
-        });
-      })
-      .catch(err => console.log(err));
+    this.props.getPosts();
   }
   render() {
-    let recentPostsMarkup = this.state.posts ? (
-      this.state.posts.map(post => <Post key={post.postId} post={post} />)
+    const { posts, loading } = this.props.data;
+    let recentPostsMarkup = !loading ? (
+      posts.map(post => <Post key={post.postId} post={post} />)
     ) : (
       <p>Loading...</p>
     );
@@ -31,11 +26,20 @@ class home extends Component {
           {recentPostsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <Profile/>
+          <Profile />
         </Grid>
       </Grid>
     );
   }
 }
 
-export default home;
+home.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  data: state.data
+});
+
+export default connect(mapStateToProps, { getPosts })(home);
