4373015b61cef149ea2e2ece7c2d6d78f5255a44
Kamrul Hossain <kam.hossain5@gmail.com>
2020-07-11 18:09:55 -0400

    Updates

---
+++ precache-manifest.4c4268d8f2968507c63a549ad49d90f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c79b88d98366b77a9d4466701633110",
+    "url": "/socialmedia-app/index.html"
+  },
+  {
+    "revision": "5fc53cfcb58e5070744f",
+    "url": "/socialmedia-app/static/css/main.bf38ceba.chunk.css"
+  },
+  {
+    "revision": "ca7b1c9898d6300f928e",
+    "url": "/socialmedia-app/static/js/2.233f483c.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/socialmedia-app/static/js/2.233f483c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc53cfcb58e5070744f",
+    "url": "/socialmedia-app/static/js/main.69264b09.chunk.js"
+  },
+  {
+    "revision": "cf1862a55321e37fc889",
+    "url": "/socialmedia-app/static/js/runtime-main.aff695a2.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/socialmedia-app/static/media/no-img.5a021ab9.png"
+  }
+]);
